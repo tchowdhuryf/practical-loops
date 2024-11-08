932912a173adc22246c8052e8f79851c4f781f2d
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2024-11-08 13:41:48 -0500

    Part 2 Prime Time

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-//Problem 1 Fizz Buzz
+//Part 1 Fizz Buzz
+// Loop through all numbers from 1 to 100.
+// If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by 5, log “Buzz.”
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// If a number is not divisible by either 3 or 5, log the number.
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0) {
         console.log("Fizz");
@@ -12,4 +17,40 @@ for (let i = 1; i <= 100; i++) {
         console.log("Fizz Buzz");
     } 
 }
+
+//Part 2 Prime Time
+// Declare an arbitrary number, n.
+// Create a loop that searches for the next prime number, starting at n and incrementing from there.
+// As soon as you find the prime number, log that number and exit the loop.
+num = 101;
+function isPrime(num) {
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    
+    return true;
+};
+
+let nextNum = num + 1;
+
+while (!isPrime(nextNum)) {
+    nextNum++;
+}
+
+console.log(nextNum);
+
+//Part 3 Feeling Loopy
+// Loop through the characters of a given CSV string.
+// Store each “cell” of data in a variable.
+// When you encounter a comma, move to the next cell.
+// When you encounter the “\r\n” sequence, move to the next “row.”
+// Log each row of data.
+// You do not need to format the data, the following works well.
+// console.log(cell1, cell2, cell3, cell4);
+//
+// You can make the following assumptions:
+// There will only be 4 cells per row.
+// There will be no escaped characters other than “\n”.
 
