518d82e0cf21713672df053871fef96c854f2a11
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2024-11-18 10:29:19 -0500

    In class progress

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ for (let i = 1; i <= 100; i++) {
     } 
 }
 
+console.log(""); //spacing between the parts
+
 //Part 2 Prime Time
 // Declare an arbitrary number, n.
 // Create a loop that searches for the next prime number, starting at n and incrementing from there.
@@ -40,6 +42,8 @@ while (!isPrime(nextNum)) {
 }
 
 console.log(nextNum);
+
+console.log(""); //spacing between the parts
 
 //Part 3 Feeling Loopy
 // Loop through the characters of a given CSV string.
@@ -66,6 +70,62 @@ let currentChar = 0;
 
 while (currentChar < csvData.length) {
     const char = csvData[currentChar];
+
+    if (char === ',') {
+        currentCell++;
+
+    } else if (char === '\n' || (char === '\r' && csvData[currentChar + 1] === '\n')) {
+        console.log(cell1, cell2, cell3, cell4);
+
+        cell1 = '';
+        cell2 = '';
+        cell3 = '';
+        cell4 = '';
+        currentCell = 1;
+
+        if (char === '\r') {
+          currentChar++;
+      }
+
+    } else {
+        switch (currentCell) {
+            case 1:
+                cell1 += char;
+                break;
+            case 2:
+                cell2 += char;
+                break;
+            case 3:
+                cell3 += char;
+                break;
+            case 4:
+                cell4 += char;
+                break;
+        }
+    }
+
+    currentChar++;
+}
+
+if (cell1 || cell2 || cell3 || cell4) {
+    console.log(cell1, cell2, cell3, cell4);
+}
+
+console.log(""); //spacing between the parts
+
+//testing another set of data
+const csvData1 = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+
+cell1 = '';
+cell2 = '';
+cell3 = '';
+cell4 = '';
+currentCell = 1; 
+currentChar = 0; 
+
+
+while (currentChar < csvData1.length) {
+    const char = csvData1[currentChar];
 
     if (char === ',') {
         currentCell++;
