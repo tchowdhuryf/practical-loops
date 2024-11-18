48da6dce8f24630dcbbe164b66e13179ee8a21ff
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2024-11-17 22:10:56 -0500

    Part 3 ALab

--- index.js
+++ index.js
@@ -54,3 +54,57 @@ console.log(nextNum);
 // There will only be 4 cells per row.
 // There will be no escaped characters other than “\n”.
 
+const csvData = "42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
+
+let cell1 = '';
+let cell2 = '';
+let cell3 = '';
+let cell4 = '';
+let currentCell = 1; 
+let currentChar = 0; 
+
+
+while (currentChar < csvData.length) {
+    const char = csvData[currentChar];
+
+    if (char === ',') {
+        currentCell++;
+
+    } else if (char === '\n' || (char === '\r' && csvData[currentChar + 1] === '\n')) {
+        console.log(cell1, cell2, cell3, cell4);
+
+        cell1 = '';
+        cell2 = '';
+        cell3 = '';
+        cell4 = '';
+        currentCell = 1;
+
+        if (char === '\r') {
+          currentChar++;
+      }
+
+    } else {
+        switch (currentCell) {
+            case 1:
+                cell1 += char;
+                break;
+            case 2:
+                cell2 += char;
+                break;
+            case 3:
+                cell3 += char;
+                break;
+            case 4:
+                cell4 += char;
+                break;
+        }
+    }
+
+    currentChar++;
+}
+
+if (cell1 || cell2 || cell3 || cell4) {
+    console.log(cell1, cell2, cell3, cell4);
+}
+
+
